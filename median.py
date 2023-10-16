1f5b2d9a3fb8fed439ab030b2d88ace0f3df5883
Yara Niroukh <yara.niroukh@kcl.ac.uk>
2023-10-16 01:33:06 +0100

    Complete exercise

--- median.py
+++ median.py
@@ -1,6 +1,15 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def median(numbersList):
+    numbersList = sorted(numbersList)
+    if len(numbersList) % 2 == 1: # odd number length
+        return numbersList[len(numbersList)//2]
+    else: # even number length
+        num1 = numbersList[len(numbersList)//2 - 1]
+        num2 = numbersList[len(numbersList)//2]
+        return (num1 + num2)/2
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +18,4 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(median(numbers))
